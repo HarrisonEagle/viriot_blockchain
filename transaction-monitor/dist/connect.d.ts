d3718a27e1ae8ded5fee1cd1f5dfe173f3f1fbb1
harrisonowl <hyzitbios@hotmail.com>
2024-05-06 22:28:27 +0900

    fix: delete unnecessary files

--- transaction-monitor/dist/connect.d.ts
+++
@@ -1,5 +0,0 @@
-import * as grpc from '@grpc/grpc-js';
-import { Identity, Signer } from '@hyperledger/fabric-gateway';
-export declare function newGrpcConnection(): Promise<grpc.Client>;
-export declare function newIdentity(): Promise<Identity>;
-export declare function newSigner(): Promise<Signer>;
